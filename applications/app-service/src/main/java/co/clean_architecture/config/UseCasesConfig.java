f48a117c8f32c91d595395ea738da2b5a6cf5ae2
Gibson-Arbey <gibsonarbey@gmail.com>
2026-04-25 12:31:10 -0500

    first commit

---
+++ applications/app-service/src/main/java/co/clean_architecture/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.clean_architecture.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.clean_architecture.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
